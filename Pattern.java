d1735717d832981879859ca293c7244ecf710090
Navya Annamreddy <navyareddie2005@gmail.com>
2025-12-18 18:38:21 +0530

    Add Pattern class to print asterisks
    Implement a pattern printing program in Java.

---
+++ Pattern.java
@@ -0,0 +1,16 @@
+// Source code is decompiled from a .class file using FernFlower decompiler (from Intellij IDEA).
+public class Pattern {
+   public Pattern() {
+   }
+
+   public static void main(String[] var0) {
+      for(int var1 = 1; var1 <= 5; ++var1) {
+         for(int var2 = var1; var2 >= 1; --var2) {
+            System.out.print("*");
+         }
+
+         System.out.println(" ");
+      }
+
+   }
+}
