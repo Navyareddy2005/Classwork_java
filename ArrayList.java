2f3026834844cc9ea9e30a794cdd0f493f779d1c
Navya Annamreddy <navyareddie2005@gmail.com>
2025-12-20 13:40:30 +0530

    Implement Student class and ArrayList example

---
+++ ArrayList.java
@@ -0,0 +1,58 @@
+import java.util.*;
+
+class Student
+{
+	private String name;
+	private int age;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public Student(String name, int age) {
+		super();
+		this.name = name;
+		this.age = age;
+	}
+	public String toString()
+	{
+		return "Name = " + name + ", Age =" + age;
+	}
+	
+}
+
+	
+
+
+public class ArrayList {
+
+	public static void main(String[] args) {
+		
+		Student s1 = new Student("Navya",69);
+		Student s2 = new Student("Chinnu",108);
+		
+		ArrayList<Student> al = new ArrayList<Student>();
+		al.add(s1);
+		al.add(s2);
+		
+		for(Object i : al)
+		{
+			System.out.println(i);
+		}
+		int a=10;
+		Integer i=new Integer(a);
+		System.out.println(i);
+		Integer in=new Integer(10);
+		int b=in;
+		System.out.println(b);
+
+	}
+
+}
